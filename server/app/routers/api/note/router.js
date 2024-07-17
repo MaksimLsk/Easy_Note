bfba5204cbe42c1acc0bcddfc33dca158a55fcdd
Maksim Losjuk <m.lysuk@gmail.com>
2024-07-17 10:48:50 +0200

    database created

--- server/app/routers/api/note/router.js
+++ server/app/routers/api/note/router.js
@@ -6,16 +6,16 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Import item-related actions
+// Import note-related actions
 const { browse, read, add } = require("../../../controllers/NoteActions");
 
-// Route to get a list of items
+// Route to get a list of notes
 router.get("/", browse);
 
-// Route to get a specific item by ID
+// Route to get a specific note by ID
 router.get("/:id", read);
 
-// Route to add a new item
+// Route to add a new note
 router.post("/", add);
 
 /* ************************************************************************* */
